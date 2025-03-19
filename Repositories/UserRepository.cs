3b732a0fd1789c7f7fb42b1f654263353e434d9b
DiegoHernandez <diego0930@gmail.com>
2025-03-19 14:29:07 -0500

    Creacion de Repository, Servicies

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,59 @@
+﻿using MiAppUsuarios.DataContext;
+using MiAppUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+namespace MiAppUsuarios.Repositories
+{
+    public class UserRepository
+    {
+
+        private readonly MiApiUsuariosDbContext _context;
+
+        public UserRepository(MiApiUsuariosDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User?> UpdateAsync(User user)
+        {
+            if (!await ExistsAsync(user.Id))
+                return null;
+
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return false;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _context.Users.AnyAsync(e => e.Id == id);
+        }
+    }
+}
